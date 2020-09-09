c129b962b8ba64a9e5e2484580ca83b93a7d9e9b
john <jgh2102@gmail.com>
2020-09-10 02:21:18 +0900

    finished phase 2

--- public/events.js
+++ public/events.js
@@ -1,8 +1,34 @@
 window.addEventListener("DOMContentLoaded", event => {
-    fetch("/kitten/image").then(res => {
-        return res.json();
-    }).then(data => {
+    fetch("/kitten/image")
+        .then(handleResponse)
+        .then(data => {
         console.log(data);
         document.querySelector(".cat-pic").src = data.src;
     })
 });
+
+let button = document.getElementById('new-pic');
+button.addEventListener('click', event =>{
+    fetch('/kitten/image').then(res=>{
+        return res.json();
+    }).then(data=>{
+        document.querySelector('.cat-pic').src = data.src;
+    })
+})
+
+const startLoading = () => {
+    document.querySelector('.loader').innerHTML = 'loading';
+    
+}
+
+const endLoading = () => {
+    document.querySelector('.loader').innerHTML = '';
+}
+
+const handleResponse = (res) =>{
+    endLoading();
+    if(!res.ok){
+        throw res;
+    }
+    return res.json();
+}
