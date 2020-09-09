1ec5513abc445903a45d90c2e87a287d3bf7a936
john <jgh2102@gmail.com>
2020-09-10 02:58:59 +0900

    phase 4 complete

--- public/events.js
+++ public/events.js
@@ -1,26 +1,3 @@
-window.addEventListener("DOMContentLoaded", event => {
-    fetch("/kitten/image")
-        .then(handleResponse)
-        .then(data => {
-        console.log(data);
-        document.querySelector(".cat-pic").src = data.src;
-        })
-        .catch(handleError)
-});
-
-
-
-let button = document.getElementById('new-pic');
-button.addEventListener('click', event =>{
-    fetch('/kitten/image')
-        .then(res=>{
-        return res.json();
-        })
-        .then(data=>{
-        document.querySelector('.cat-pic').src = data.src;
-        })
-        .catch(handleError)
-})
 
 const startLoading = () => {
     document.querySelector('.loader').innerHTML = 'loading';
@@ -42,3 +19,44 @@ const handleResponse = (res) =>{
 const handleError = (error) => {
     window.alert("Something went wrong. Please try again.")
 }
+
+const upVote = () =>{
+    fetch('/kitten/upvote', {method: 'PATCH'})
+    .then(handleResponse)
+    .then(data => {
+        const {score} = data;
+        document.querySelector('.score').innerHTML = score;
+    })
+    .catch(handleError);
+}
+
+const downVote = () =>{
+    fetch('/kitten/downvote', {method: 'PATCH'})
+    .then(handleResponse)
+    .then(data => {
+        const {score} = data;
+        document.querySelector('.score').innerHTML = score;
+    })
+    .catch(handleError);
+}
+
+const getImage = () =>{
+    startLoading();
+    fetch("/kitten/image")
+    .then(handleResponse)
+    .then(data => {
+    console.log(data);
+    document.querySelector(".cat-pic").src = data.src;
+    })
+    .catch(handleError)
+}
+
+
+window.addEventListener("DOMContentLoaded", getImage);
+
+
+document.getElementById('upvote').addEventListener('click', upVote);
+document.getElementById('downvote').addEventListener('click', downVote)
+
+let button = document.getElementById('new-pic');
+button.addEventListener('click', getImage);
